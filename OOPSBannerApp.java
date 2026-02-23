e72cd3356cd4901a0d930f490d1b5a33d1d228e1
ss1969-tech <ss1969@srmist.edu.in>
2026-02-23 13:12:23 +0530

    usecase1

---
+++ OOPSBannerApp.java
@@ -0,0 +1,11 @@
+/**
+ * OOPSBannerApp
+ * @author sanjeev
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to OOPS Programming");
+    }
+}
