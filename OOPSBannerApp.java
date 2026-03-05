0e8e8f790cf4ee58763e39004c5b73fd7f257094
ss1969-tech <ss1969@srmist.edu.in>
2026-03-05 21:52:23 +0530

    usecase7

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,19 +1,18 @@
-/*
- * OOPSBannerApp UC2 – Banner Display Application
- * Displays "OOPS" using ASCII art with * and spaces
- * Using + operator for string concatenation
- */
-
 public class OOPSBannerApp {
-
     public static void main(String[] args) {
 
-        System.out.println("  ***   " + "  ***   " + " *****  " + " *****  ");
-        System.out.println(" *   *  " + " *   *  " + " *    * " + " *      ");
-        System.out.println("*     * " + "*     * " + " *    * " + " *      ");
-        System.out.println("*     * " + "*     * " + " *****  " + "  ***** ");
-        System.out.println("*     * " + "*     * " + " *      " + "       *");
-        System.out.println(" *   *  " + " *   *  " + " *      " + " *     *");
-        System.out.println("  ***   " + "  ***   " + " *      " + "  ***** ");
+        String[] lines = new String[7];
+
+        lines[0] = String.join(" ", " * ", " * ", " ** ", " ** ");
+        lines[1] = String.join(" ", "*   ", "   ", "   ",  "     ");
+        lines[2] = String.join(" ", "*   ", "   ", "   ",  "     ");
+        lines[3] = String.join(" ", "*   ", "   ", "* ",  " ** ");
+        lines[4] = String.join(" ", "*   ", "   ", "    ",  "    * ");
+        lines[5] = String.join(" ", "*   ", "   ", "    ",  "    * ");
+        lines[6] = String.join(" ", " * ", " * ", "*    ",  "*** ");
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
     }
 }
